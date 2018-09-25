958a9ec40f067c601a3581b0a95c2a20f56a5cfd
Daniel Roger Hansen <daniel.brann@hotmail.no>
2018-09-25 12:23:50 +0200

    Obligatorisk oppgave i programmering

--- src/obligatoriskopgave1/ObligatoriskOpgave1.java
+++ src/obligatoriskopgave1/ObligatoriskOpgave1.java
@@ -4,6 +4,7 @@ public class ObligatoriskOpgave1 {
     public static void main(String[] args) {
      System.out.println("Obligatorisk oppgave 1" + '\n' + 
                         "Daniel Roger Hansen - s333748" + '\n' +
+                        //Fyll inn navn og studentnummer under
                         "... - ..." + '\n' + 
                         "... - ...");
     } 
